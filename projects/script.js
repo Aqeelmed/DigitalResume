f240b9a7d2f29600c37136eb97acf036be2c14a8
Aqeel Ahmad <126767765+Aqeelmed@users.noreply.github.com>
2026-04-26 15:18:51 +0500

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Aqeel Ahmad";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
